7061adef3023f87eb134b756c833269431bb6ffa
Alice Rühl <alice.ruehl@gmail.com>
2020-11-10 17:36:26 +0100

    Upgrade to React 17

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,4 +4,3 @@
 module.exports.hello = require('./hello');
 module.exports.Dropdown = require('./dropdown');
 module.exports.Modal = require('./modal');
-module.exports.ScrollArea = require('./scrollarea');
