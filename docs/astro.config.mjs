4b5954349bcc6c35677bbb38ee3e9ad0758d7d12
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-03-18 23:25:38 +0100

    started setup structure for project

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,31 +1,58 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Overview',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Design System Overview', slug: 'overview' },
-					],
-				},
-				{
-					label: 'Components',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Components Overview', slug: 'components' },
-					],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "OpenAQ Design System",
+      logo: {
+        src: "./src/assets/logo.svg",
+        replacesTitle: true,
+      },
+
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            href: "/favicon.ico",
+            sizes: "16x16 32x32",
+          },
+        },
+      ],
+      components: {
+        Head: "/src/components/Head.astro",
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      customCss: ["./src/styles/main.scss"],
+      sidebar: [
+        {
+          label: "Overview",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Design System Overview", slug: "overview" },
+          ],
+        },
+        {
+          label: "Components",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Components Overview", slug: "components" },
+            { label: "Badges", slug: "components/badges" },
+          ],
+        },
+        {
+          label: "Utilities",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Utilities Overview", slug: "utilities" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
