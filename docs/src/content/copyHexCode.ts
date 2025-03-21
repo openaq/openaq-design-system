aaf335107f4a2caeb7de6d28eb86a45c2910ac5b
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-03-21 15:53:15 +0100

    create clearText function + added smoke180 for innerHTML to most transparent color circles

--- docs/src/content/copyHexCode.ts
+++ docs/src/content/copyHexCode.ts
@@ -9,7 +9,6 @@ type colorPaletteType = {
 
 const printColorPalette = (colorPalettes: colorPaletteType[]) => {
   const colorWrapper = document.querySelector("#testWrapper");
-  console.log("HEJ!", colorPalettes);
 
   if (!colorWrapper) {
     console.log("Can't find en element with id colorPaletteWrapper");
@@ -19,13 +18,20 @@ const printColorPalette = (colorPalettes: colorPaletteType[]) => {
   colorPalettes.map((color: colorPaletteType) => {
     colorWrapper.innerHTML += `
       <div class="${color.class}" data-id="${color.hexCode}">
-        <span style="color: white">${color.tone}</span>
+        <span style="${
+          /\b10\b/.test(color.class) ? "color: #30363c;" : "color: #ffffff;"
+        }">${color.tone}</span>
       </div>
     `;
   });
 };
 
+(() => {
+  printColorPalette(colorPalettes);
+})();
+
 const getHexCode = (e: Event) => {
+  clearText();
   const target = e.currentTarget as HTMLElement;
   const circleObject = colorPalettes.find(
     (item) => item.hexCode === target.dataset.id
@@ -36,16 +42,38 @@ const getHexCode = (e: Event) => {
   } else {
     navigator.clipboard.writeText(newCode);
     target.innerHTML = "Hex code copied!";
-    target.style.color = "white";
     target.style.fontSize = "10px";
+
+    const className = target.className;
+
+    if (/\b10\b/.test(className)) {
+      target.style.color = "#30363c";
+    } else {
+      target.style.color = "#ffffff";
+    }
   }
 };
 
-(() => {
-  printColorPalette(colorPalettes);
-})();
-
-const circles = document.querySelectorAll(".circle");
+const circles = document.querySelectorAll(".color-circle");
 circles.forEach((btn) => {
   btn.addEventListener("click", getHexCode);
 });
+
+const clearText = () => {
+  const allCircles = document.querySelectorAll(".color-circle");
+  const allColorPalettes = colorPalettes;
+  allCircles.forEach((circle) => {
+    if (circle.innerHTML === "Hex code copied!") {
+      const circleDataId = circle.getAttribute("data-id");
+
+      allColorPalettes.forEach((color) => {
+        if (circleDataId === color.hexCode) {
+          circle.innerHTML = `${color.tone}`;
+          return;
+        } else {
+          return;
+        }
+      });
+    }
+  });
+};
